93b7c660f31cd278601ba2da29dbd64169eeef4d
lingzy <lingzhengying@outlook.com>
2017-09-14 15:56:24 +0800

    reduce some url

--- only1word/urls.py
+++ only1word/urls.py
@@ -13,9 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf.urls import include
+from oneword import views
+
+# from django.contrib.auth.views import login,logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',views.home,name='home'),
+    url(r'^popular/$', views.popular,name='popular'),
+    url(r'^create/$', views.create),
+    url(r'^test/$', views.test),
+    # url(r'^newar/$', views.newar),
+    url(r'^add_comment/$', views.add_comment),
+    url(r'^api/', include('oneword.urls',namespace='oneword')),
+    url(r'^profiles/$', views.userprofiles,name='profiles'),
+
+    # url('^', include('django.contrib.auth.urls')),
 ]
+
