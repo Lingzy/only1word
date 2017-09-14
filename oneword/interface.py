36c7b25202fc8b217996404a46b5a7ef425ec672
lingzy <lingzhengying@outlook.com>
2017-09-14 10:11:36 +0800

    add exist interface file

---
+++ oneword/interface.py
@@ -0,0 +1,82 @@
+from message.models import Article,Comment
+from django.http import JsonResponse
+from django.core.exceptions import ValidationError,ObjectDoesNotExist
+
+
+# 返回文章列表接口
+def allarticle(request):
+    article_list=[]
+    all_article = Article.objects.all()
+    for article in all_article:
+        data = {}
+        data['author'] = article.author.username
+        data['title'] = article.title
+        data['tag'] = article.tag
+        article_list.append(data)
+
+    return JsonResponse({'status':200,'article_list':article_list,'message':'success'})
+
+
+# 根据作者返回文章信息接口
+def get_article_author(request):
+    # title = request.POST.get("title",'')
+    author = request.POST.get('author','')
+
+    if author == '':
+        return JsonResponse({'status':10021,'message':'parameter error'})
+
+    articles = Article.objects.filter(author__username = author.lower())
+    article_list=[]
+    if articles:
+        for article in articles:
+            data = {}
+            data['title'] = article.title
+            data['tag'] = article.tag
+            data['id'] = article.id
+            article_list.append(data)
+        return JsonResponse({'status':200,'data':article_list,'author':author,'message':'success'})
+    return JsonResponse({'status':10022,'message':'No result'})
+
+# 根据文章名称返回文章信息
+def get_article_title(request):
+
+    title = request.POST.get('title','')
+
+    if title == '':
+        return JsonResponse({'status':10021,'message':'parameter error'})
+
+
+    articles = Article.objects.filter(title__contains = title.lower())
+    article_list=[]
+    if articles:
+        for article in articles:
+            data={}
+            data['title'] = article.title
+            data['author'] = article.author.username
+            data['tag'] = article.tag
+            data['id'] = article.id
+            article_list.append(data)
+        return JsonResponse({'status':200,'data':article_list,'message':'success'})
+    return JsonResponse({'status':10022,'message':'No result'})
+
+def article_like(request):
+    if request.method == 'POST':
+        title = request.post.get('title','')
+
+        if title == '':
+            return JsonResponse({'status':10021,'message':'parameter error'})
+
+        article = Article.objects.get(title=title)
+        article.article_likes += 1
+        article.save()
+
+def article_dislike(request):
+    if request.method == 'POST':
+        title = request.post.get('id','')
+
+        if title == '':
+            return JsonResponse({'status':10021,'message':'parameter error'})
+
+        article = Article.objects.get(title=title)
+        article.article_dislikes += 1
+        article.save()
---
+++ oneword/static/js/notalong.js
@@ -0,0 +1,136 @@
+$(document).ready(function(){
+  $(".title_error").hide();
+  $("#create_button").attr("disabled",false);
+
+  $('.article ').click(function(){
+    var x = $(this).attr("id");
+    $('[article_id='+ x +']').modal('show');
+  });
+
+
+  $('#create').click(function(){
+    $('#create_new').modal('show');
+  });
+
+
+  $('#article_title').blur(function(){
+    var title = $(this).val();
+    $.post("/api/title_search/",{"title":title},function(result){
+      if (result.status == 200){
+        $(".title_error").show();
+        $("#create_button").attr("disabled",true);
+      }
+      else{
+        $(".title_error").hide();
+        $("#create_button").attr("disabled",false);
+      }
+    })
+  });
+
+
+  $("#id_username").focus(function(){
+    $(".errorlist").hide();
+  });
+
+
+  $('.ui.form')
+  .form({
+    on:'blur',
+    fields:{
+      title:{
+        indentifier : 'title',
+        rules:[
+          {
+            type:'empty',
+            prompt:'Please enter a title'
+          }
+        ]
+      },
+      tags:{
+        indentifier: 'tags',
+        rules:[
+          {
+            type:'empty',
+            prompt:'Please enter tags'
+          }
+        ]
+      },
+      newcontent: {
+        identifier  : 'newcontent',
+        rules: [
+          {
+            type   : 'minLength[15]',
+            prompt : 'Please enter at least 15 characters'
+          },
+          {
+            type   : 'maxLength[150]',
+            prompt : 'Please enter at most 150 characters'
+          }
+        ]
+      },
+      follow_content: {
+        identifier  : 'follow_content',
+        rules: [
+          {
+            type   : 'minLength[15]',
+            prompt : 'Please enter at least 15 characters'
+          },
+          {
+            type   : 'maxLength[60]',
+            prompt : 'Please enter at most 150 characters'
+          }
+        ]
+      },
+    }
+  });
+
+
+  $('.menu .item')
+    .tab()
+  ;
+
+
+  $('.prompt').change(function(){
+    var strings = $(this).val();
+    var titlesearch=0;
+    var authorsearch=0;
+    if (strings){
+      $('.card').hide();
+      $.post("/api/title_search/",{"title":strings},function(result){
+        if (result.status == 200){
+          // var id = result.data["id"];
+          article = result.data;
+          for (var i=0, len=article.length; i<len;i++){
+            $("#"+article[i].id).parent('.card').show();
+          }
+        }
+        else {
+          titlesearch=1;
+          if (titlesearch === 1 && authorsearch === 1){
+            alert("No result");
+          }
+        }
+      });
+      $.post("/api/author_search/",{"author":strings},function(result){
+        if (result.status == 200){
+          // var id = result.data["id"];
+          article = result.data;
+          for (var i=0, len=article.length; i<len;i++){
+            $("#"+article[i].id).parent('.card').show();
+          }
+        }
+        else {
+          authorsearch=1;
+          if (titlesearch === 1 && authorsearch === 1){
+            alert("No result");
+          }
+        }
+      });
+    }
+
+    else{
+      $('.card').show();
+      alert("Input title or author name to search");
+    }
+  });
+});
