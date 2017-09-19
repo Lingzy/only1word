27b73f3816d81df275650564a41100a17cb2cd57
lingzy <lingzhengying@outlook.com>
2017-09-19 11:12:51 +0800

    增加后台管理

--- oneword/admin.py
+++ oneword/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from oneword.models import Article,Comment
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title','author','create_time']
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author','create_time']
+
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(Comment,CommentAdmin)
 
 # Register your models here.
