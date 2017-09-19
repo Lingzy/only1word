03fbab8ee69809d5100a114bfbc5596f88ba17e3
lingzy <lingzhengying@outlook.com>
2017-09-19 11:31:56 +0800

    修改为articleinterface.py

--- oneword/articleinterface.py
+++ oneword/articleinterface.py
@@ -1,6 +1,7 @@
-from oneword.models import Article,Comment,MyFavorite
+from oneword.models import Article,Comment,MyFavorite,MyLike
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ValidationError,ObjectDoesNotExist
 
 
@@ -21,7 +22,7 @@ def allarticle(request):
 
 # 根据作者返回文章信息接口
 def get_article_author(request):
-    author = request.POST.get('author','')
+    author = request.GET.get('author','')
 
     if author == '':
         return JsonResponse({'status':10021,'message':'parameter error'})
@@ -41,7 +42,7 @@ def get_article_author(request):
 # 根据文章名称返回文章信息
 def get_article_title(request):
 
-    title = request.POST.get('title','')
+    title = request.GET.get('title','')
 
     if title == '':
         return JsonResponse({'status':10021,'message':'parameter error'})
@@ -60,7 +61,37 @@ def get_article_title(request):
     return JsonResponse({'status':10022,'message':'No result'})
 
 # 喜欢文章接口
+
 def article_like(request):
+    if request.method == 'GET':
+        id = request.GET.get('extra_id','')
+        username = request.session.get('user','')
+
+        if not username:
+            return JsonResponse({'status':10020,'message':'Please sign in first'})
+
+        if id == '':
+            return JsonResponse({'status':10021,'message':'parameter error'})
+
+        # 添加喜欢
+        article = Article.objects.get(id=id)
+        user = User.objects.get(username=username)
+        mylike = MyLike.objects.get(collector=user)
+        mylike.like.add(article)
+        mylike.save()
+
+        # 增加点赞数
+        article.like += 1
+        article.save()
+
+        return JsonResponse({'status':200,'article_id':article.id,'like':article.like,'message':'add like success'})
+
+    return JsonResponse({'status':10022,'message':'not GET method'})
+
+
+# 收藏文章接口
+
+def article_favorite(request):
     if request.method == 'GET':
         id = request.GET.get('extra_id','')
         username = request.session.get('user','')
@@ -78,24 +109,7 @@ def article_like(request):
         myfavorite.collection.add(article)
         myfavorite.save()
 
-        # 增加点赞数
-        article.like += 1
+        article.favorite += 1
         article.save()
-
-        return JsonResponse({'status':200,'article_id':article.id,'like':article.like,'message':'add like success'})
-
-    return JsonResponse({'status':10022,'message':'not GET method'})
-
-# 不喜欢文章接口
-def article_dislike(request):
-    if request.method == 'GET':
-        id = request.GET.get('extra_id','')
-
-        if id == '':
-            return JsonResponse({'status':10021,'message':'parameter error'})
-
-        article = Article.objects.get(id=id)
-        article.dislike += 1
-        article.save()
-        return JsonResponse({'status':200,'article_id':article.id,'dislike':article.dislike,'message':'add dislike success'})
+        return JsonResponse({'status':200,'article_id':article.id,'favorite':article.favorite,'message':'add dislike success'})
     return JsonResponse({'status':10022,'message':'not GET method'})
