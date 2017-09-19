9ff98273efed7f480feacda686bf182f6c3b5a29
lingzy <lingzhengying@outlook.com>
2017-09-19 11:27:44 +0800

    修改interface.py为articleinterface.py

--- oneword/urls.py
+++ oneword/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
-from oneword import interface
+from oneword import articleinterface
 from oneword import userinfo
 
 
 urlpatterns = [
-    url(r'^all_list/', interface.allarticle,name='article_list'),
-    url(r'^author_search/',interface.get_article_author,name='get_article_author'),
-    url(r'^title_search/',interface.get_article_title,name='get_article_title'),
-    url(r'^like/', interface.article_like,name='like'),
-    url(r'^dislike', interface.article_dislike,name='dislike'),
+    url(r'^all_list/', articleinterface.allarticle,name='article_list'),
+    url(r'^author_search/',articleinterface.get_article_author,name='get_article_author'),
+    url(r'^title_search/',articleinterface.get_article_title,name='get_article_title'),
+    url(r'^like/', articleinterface.article_like,name='like'),
+    url(r'^favorite', articleinterface.article_favorite,name='favorite'),
     url(r'^sign/',userinfo.sign,name = 'user_sign'),
     url(r'^register/',userinfo.register,name='user_register'),
     url(r'^logout/',userinfo.userlogout,name='user_logout'),
