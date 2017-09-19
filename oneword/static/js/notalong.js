49bccb44b968d6d826c270469f046ec0ef03a79e
lingzy <lingzhengying@outlook.com>
2017-09-19 15:01:14 +0800

    重写ajax，改用同步

--- oneword/static/js/notalong.js
+++ oneword/static/js/notalong.js
@@ -2,17 +2,18 @@ $(document).ready(function(){
   $(".title_error").hide();
   $("#create_button").attr("disabled",false);
 
+  // 点击文章显示文章与评论区modal
   $('.article ').click(function(){
     var x = $(this).attr("id");
     $('[article_id='+ x +']').modal('show');
   });
 
-
+ // 创建文章modal显示
   $('#create').click(function(){
     $('#create_new').modal('show');
   });
 
-
+  // 判断文章title是否重复
   $('#article_title').blur(function(){
     var title = $(this).val();
     $.post("/api/title_search/",{"title":title},function(result){
@@ -27,16 +28,17 @@ $(document).ready(function(){
     })
   });
 
-
+  // 聚焦用户名输入框时隐藏错误提示信息
   $("#id_username").focus(function(){
     $(".errorlist").hide();
   });
 
-
+  // 创建文章form表单验证
   $('.ui.form')
   .form({
     on:'blur',
     fields:{
+      // 标题为空验证
       title:{
         indentifier : 'title',
         rules:[
@@ -46,6 +48,7 @@ $(document).ready(function(){
           }
         ]
       },
+      // tags为空验证
       tags:{
         indentifier: 'tags',
         rules:[
@@ -55,6 +58,7 @@ $(document).ready(function(){
           }
         ]
       },
+      // 文章内容为空，长度限制验证
       newcontent: {
         identifier  : 'newcontent',
         rules: [
@@ -68,8 +72,9 @@ $(document).ready(function(){
           }
         ]
       },
-      follow_content: {
-        identifier  : 'follow_content',
+      // 评论为空，长度限制验证
+      comment_content: {
+        identifier  : 'comment_content',
         rules: [
           {
             type   : 'minLength[15]',
@@ -84,48 +89,78 @@ $(document).ready(function(){
     }
   });
 
+  // 设置ajax同步请求
 
-  $('.menu .item')
-    .tab()
-  ;
 
-// 搜索文章
+  // 搜索文章
   $('.prompt').change(function(){
     var strings = $(this).val().replace(/\ +/g,"");
     var titlesearch=0;
     var authorsearch=0;
+    var tagsearch=0;
     if (strings){
       $('.card').hide();
-      $.get("/api/title_search/",{"title":strings},function(result){
-        if (result.status == 200){
-          // var id = result.data["id"];
-          article = result.data;
-          for (var i=0, len=article.length; i<len;i++){
-            $("#"+article[i].id).parent('.card').show();
-          }
-        }
-        else {
-          titlesearch=1;
-          if (titlesearch === 1 && authorsearch === 1){
-            $('.empty.modal').modal('show');
-          }
-        }
-      });
-      $.get("/api/author_search/",{"author":strings},function(result){
-        if (result.status == 200){
-          // var id = result.data["id"];
-          article = result.data;
-          for (var i=0, len=article.length; i<len;i++){
-            $("#"+article[i].id).parent('.card').show();
-          }
-        }
-        else {
-          authorsearch=1;
-          if (titlesearch === 1 && authorsearch === 1){
-            $('.empty.modal').modal('show');
-          }
-        }
-      });
+      // 通过标题搜索
+      $.ajax({url:"/api/title_search/",
+              async:false,
+              data:{"title":strings},
+              success:function(result){
+                if (result.status == 200){
+                  // var id = result.data["id"];
+                  article = result.data;
+                  for (var i=0, len=article.length; i<len;i++){
+                    $("#"+article[i].id).parent('.card').show();
+                  }
+                }
+                else {
+                  titlesearch=1;
+                  // if (titlesearch === 1 && authorsearch === 1){
+                  //   $('.empty.modal').modal('show');
+                  // }
+                }
+              }});
+      $.ajax({url:"/api/author_search/",
+              async:false,
+              data:{"author":strings},
+              success:function(result){
+                if (result.status == 200){
+                  // var id = result.data["id"];
+                  article = result.data;
+                  for (var i=0, len=article.length; i<len;i++){
+                    $("#"+article[i].id).parent('.card').show();
+                  }
+                }
+                else {
+                  authorsearch=1;
+                  // if (titlesearch === 1 && authorsearch === 1){
+                  //   $('.empty.modal').modal('show');
+                  // }
+                }
+              }});
+
+      // 通过tags搜索
+      $.ajax({url:"/api/tag_search/",
+              async:false,
+              data:{"tags":strings},
+              success:function(result){
+                if (result.status == 200){
+                  // var id = result.data["id"];
+                  article = result.data;
+                  for (var i=0, len=article.length; i<len;i++){
+                    $("#"+article[i].id).parent('.card').show();
+                  }
+                }
+                else {
+                  tagsearch=1;
+                  // if (titlesearch === 1 && authorsearch === 1){
+                  //   $('.empty.modal').modal('show');
+                  // }
+                }
+              }});
+
+      if (titlesearch==1 && authorsearch==1 && tagsearch==1){
+        $('.empty.modal').modal('show');
+      }
     }
 
     else{
@@ -134,25 +169,45 @@ $(document).ready(function(){
     }
   });
 
+  // 添加喜欢文章
   $(".like").click(function(){
     var id = $(this).parent('.extra.content').attr('extra_id');
-    $.get("/api/like/",{"extra_id":id},function(result){
-      if (result.status == 200) {
-        $('[extra_id='+ result.article_id +']').find('a.like').html(
-          "<i class='thumbs up icon'></i>"+result.like);
-        // $('[extra_id='+ result.id +']').find('i.up').attr("class","thumbs up icon");
-      }
-      if (result.status == 10020){
-        location.href="accounts/login/"
-      }
-    })
+    $.ajax({url:"/api/like/",
+            data:{"extra_id":id},
+            async:false,
+            success:function(result){
+              if (result.status == 200) {
+                $('[extra_id='+ result.article_id +']').find('a.like').attr('class','dislike').html(
+                  "<i class='thumbs up icon'></i>"+result.like);
+                // $('[extra_id='+ result.id +']').find('i.up').attr("class","thumbs up icon");
+              }
+              if (result.status == 10020){
+                location.href="accounts/login/"
+              }
+            }});
   });
 
+  // 取消喜欢文章
+  $(".dislike").click(function(){
+    var id = $(this).parent('.extra.content').attr('extra_id');
+    $.ajax({url:"/api/dislike/",
+            data:{"extra_id":id},
+            async:false,
+            success:function(result){
+              if (result.status == 200) {
+                $('[extra_id='+ result.article_id +']').find('a.dislike').attr('class','like').html(
+                  "<i class='thumbs outline up icon'></i>"+result.like);
+                // $('[extra_id='+ result.id +']').find('i.up').attr("class","thumbs up icon");
+              }
+            }});
+  });
+
+  // 添加收藏文章
   $(".favorite").click(function(){
     var id = $(this).parent('.extra.content').attr('extra_id');
     $.get("/api/favorite/",{"extra_id":id},function(result){
       if (result.status == 200) {
-        $('[extra_id='+ result.article_id +']').find('a.favorite').html(
+        $('[extra_id='+ result.article_id +']').find('a.favorite').attr('class','unfavorite').html(
           "<i class='heart icon'></i>"+result.favorite);
         // $('[extra_id='+ result.id +']').find('i.up').attr("class","thumbs up icon");
       }
@@ -162,6 +217,19 @@ $(document).ready(function(){
     })
   });
 
+  // 取消收藏文章
+  $(".unfavorite").click(function(){
+    var id = $(this).parent('.extra.content').attr('extra_id');
+    $.get("/api/unfavorite/",{"extra_id":id},function(result){
+      if (result.status == 200) {
+        $('[extra_id='+ result.article_id +']').find('a.unfavorite').attr('class','favorite').html(
+          "<i class='empty heart icon'></i>"+result.favorite);
+        // $('[extra_id='+ result.id +']').find('i.up').attr("class","thumbs up icon");
+      }
+    })
+  });
+
+  // 用户详情页如果文章少于5个则隐藏上下页按钮
   var my_articles = $(".my_article").length;
   if (my_articles < 5){
     $('.myarticle_arrow').hide()
