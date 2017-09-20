b8522c9a7834a9cc891bca9637881ef3f2cc7880
lingzy <lingzhengying@outlook.com>
2017-09-20 18:04:12 +0800

    增加一个是否是我的文章接口

--- oneword/urls.py
+++ oneword/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^author_search/',articleinterface.get_article_author,name='get_article_author'),
     url(r'^title_search/',articleinterface.get_article_title,name='get_article_title'),
     url(r'^tag_search/',articleinterface.get_article_tag,name='get_article_tag'),
+    url(r'^myownarticle/', articleinterface.my_own_article,name='my_own_article'),
     url(r'^like/', articleinterface.article_like,name='like'),
     url(r'^dislike/', articleinterface.article_dislike,name='dislike'),
     url(r'^favorite/', articleinterface.article_favorite,name='favorite'),
@@ -15,5 +16,5 @@ urlpatterns = [
     url(r'^sign/',userinfo.sign,name = 'user_sign'),
     url(r'^register/',userinfo.register,name='user_register'),
     url(r'^logout/',userinfo.userlogout,name='user_logout'),
-    url('^changepwd/',userinfo.change_pwd,name='changepwd'),
+    url(r'^changepwd/',userinfo.change_pwd,name='changepwd'),
 ]
